9677e7fc89ab09241d3b539281429908c7df3d85
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-03-07 17:45:59 +0300

    Update image_and_icons.dart

--- lib/screens/details/components/image_and_icons.dart
+++ lib/screens/details/components/image_and_icons.dart
@@ -14,59 +14,60 @@ class ImageAndIcons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.only(bottom: kDefaultPadding*3),
+    return Padding(padding: EdgeInsets.only(bottom: kDefaultPadding*2.65),
       child: SizedBox(
-        height: size.height*0.8,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-                child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
-              child: Column(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: IconButton(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                      icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+          height: size.height*0.79,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  IconCard(icon: "assets/icons/sun.svg"),
-                  IconCard(icon: "assets/icons/icon_2.svg"),
-                  IconCard(icon: "assets/icons/icon_3.svg"),
-                  IconCard(icon: "assets/icons/icon_4.svg"),
-                ],
-              ),
-            )),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(63),
-                    bottomLeft: Radius.circular(63),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 60,
-                        color: kPrimaryColor.withOpacity(0.29))
+                    Spacer(),
+                    IconCard(icon: "assets/icons/sun.svg"),
+                    IconCard(icon: "assets/icons/icon_2.svg"),
+                    IconCard(icon: "assets/icons/icon_3.svg"),
+                    IconCard(icon: "assets/icons/icon_4.svg"),
                   ],
-                  image: DecorationImage(
-                      alignment: Alignment.centerLeft,
-                      fit: BoxFit.cover,
-                      image: AssetImage("assets/images/img.png"))),
-            )
-          ],
+                ),
+              )),
+              Container(
+                height: size.height * 0.63,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(63),
+                      bottomLeft: Radius.circular(63),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 60,
+                          color: kPrimaryColor.withOpacity(0.29))
+                    ],
+                    image: DecorationImage(
+                        alignment: Alignment.centerLeft,
+                        fit: BoxFit.fitHeight,
+                        image: AssetImage("assets/images/bottom_img_2.png"))),
+              )
+            ],
+          ),
         ),
-      ),
     );
+    
   }
 }
 
