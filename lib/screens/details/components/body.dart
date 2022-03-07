88936a17d58f7161a84fefaee34479df540df41d
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-03-07 19:51:26 +0300

    Update body.dart

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,70 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import '../../../constants.dart';
-import 'icon_card.dart';
+import 'image_and_icons.dart';
+import 'title_and_price.dart';
 
-class ImageAndIcons extends StatelessWidget {
-  const ImageAndIcons({
-    Key? key,
-    required this.size,
-  }) : super(key: key);
-
-  final Size size;
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: kDefaultPadding * 2.65),
-      child: SizedBox(
-        height: size.height * 0.79,
-        child: Row(
-          children: [//<Widget>[] same with [] -> not required type 
-            Expanded(
-                child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
-              child: Column(
-                children: <Widget>[SizedBox(height: size.height*0.01,),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: IconButton(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                      icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          TitleAndPrice(
+            title: "Angela",
+            country: "Russia",
+            price: 440,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 48,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.only(topRight: Radius.circular(20))),
+                  color: kPrimaryColor,
+                  onPressed: () {},
+                  child: Text(
+                    "Buy now",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
-                  Spacer(),
-                  IconCard(icon: "assets/icons/sun.svg"),
-                  IconCard(icon: "assets/icons/icon_2.svg"),
-                  IconCard(icon: "assets/icons/icon_3.svg"),
-                  IconCard(icon: "assets/icons/icon_4.svg"),
-                ],
+                ),
               ),
-            )),
-            Container(
-              height: size.height * 0.63,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(63),
-                    bottomLeft: Radius.circular(63),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 60,
-                        color: kPrimaryColor.withOpacity(0.29))
-                  ],
-                  image: DecorationImage(
-                      alignment: Alignment.centerLeft,
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/images/bottom_img_2.png"))),
-            )
-          ],
-        ),
+              SizedBox(width: 10),
+              Expanded(
+                  child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20))))),
+                child: Text("Description",
+                    style: TextStyle(
+                        color: Colors.white, fontSize: 18, height: 2.6)),
+              ))
+            ],
+          ),
+        ],
       ),
     );
   }
