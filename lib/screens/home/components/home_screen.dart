d3feb16f8281323cb17753cac6cafa5e950841ea
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2022-03-27 18:30:09 +0300

    added const params

--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: PreferredSize(
         child: buildAppBar(),
         preferredSize: Size.fromHeight(50.0),
