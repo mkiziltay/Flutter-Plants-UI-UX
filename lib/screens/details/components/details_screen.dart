6bd53beedfc7cf7bec2d17cd530bfa796ce8c885
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2022-03-27 18:33:12 +0300

    added const params

--- lib/screens/details/components/details_screen.dart
+++ lib/screens/details/components/details_screen.dart
@@ -6,7 +6,7 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Body(),
     );
   }
