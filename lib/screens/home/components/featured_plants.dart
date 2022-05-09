1633e335bdbe0bd9861d99f635e2881827f27d13
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2022-05-09 12:28:04 +0300

    cons mod. edded

--- lib/screens/home/components/featured_plants.dart
+++ lib/screens/home/components/featured_plants.dart
@@ -10,7 +10,13 @@ class FeaturedPlants extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(scrollDirection: Axis.horizontal,child: Row(children: <Widget>[FeaturedPlantCard(image: "assets/images/bottom_img_1.png", press: () {}),FeaturedPlantCard(image: "assets/images/bottom_img_2.png", press: () {})] ));
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturedPlantCard(image: "assets/images/bottom_img_1.png", press: () {}),
+        FeaturedPlantCard(image: "assets/images/bottom_img_2.png", press: () {})
+        ] ));
   }
 }
 
